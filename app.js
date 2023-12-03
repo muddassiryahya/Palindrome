3e75a53411eed72f3421a171fd51d0b7891efe94
muddassiryahya <m.m2442212@gmail.com>
2023-12-03 22:30:03 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// var userInput = prompt("Enter your word")
+// var data = ''
+// for(i=userInput.length-1; i>=0; i--){
+//     data+=userInput[i]
+// }
+
+// if(data===userInput){
+//     console.log(userInput + " is palindrom")
+// }
+// else{
+//     console.log(userInput + " is not palindrom")
+// }
+
