e17a544ace1869967bbba9d568d2c08e5d310d81
muddassiryahya <m.m2442212@gmail.com>
2023-12-04 22:51:58 +0500

    added some lines

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
-// var userInput = prompt("Enter your word")
+
+// First Method
+
+// var userInput = prompt("Enter any word")
 // var data = ''
 // for(i=userInput.length-1; i>=0; i--){
 //     data+=userInput[i]
 // }
 
-// if(data===userInput){
+// if(data == userInput){
 //     console.log(userInput + " is palindrom")
 // }
 // else{
 //     console.log(userInput + " is not palindrom")
 // }
 
+
+
+
+
+// Second Method
+
+// var userInput = prompt("Enter any word")
+// console.log(userInput)
+// var input = userInput.split('').reverse().join("")
+// console.log(input)
+
+// if (userInput == input) {
+//     console.log(userInput + " is palindrom")
+// } 
+// else {
+//     console.log(userInput + " is not palindrom")
+// }
